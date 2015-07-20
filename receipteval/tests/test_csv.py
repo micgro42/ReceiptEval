a3a7680a43ae51b2c8584d8c3cd8a959368513a2
Michael Große <mic.grosse+github@googlemail.com>
2015-07-20 12:28:39 +0200

    Add first test for writing csv lineendings

---
+++ receipteval/tests/test_csv.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Apr 19, 2015
+
+@author: Michael Große <mic.grosse@posteo.de>
+'''
+import pytest
+import unittest
+import csv
+
+
+class TestCSV(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_csv_write(tmpdir):
+        with open(str(tmpdir)+'testfile.csv', 'wb') as testfile:
+            csv_writer = csv.writer(testfile, lineterminator='\n')
+            csv_writer.writerow(["foo", "bar"])
+            csv_writer.writerow(["baz", "foobar"])
+        with open(str(tmpdir)+'testfile.csv', 'r') as testfile:
+            assert testfile.read() == 'foo,bar\nbaz,foobar\n'
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
