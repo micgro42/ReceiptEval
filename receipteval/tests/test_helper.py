00331a81a7e8de94c1086b8ae047b51438655234
Michael Große <mic.grosse+wikitech@googlemail.com>
2015-09-14 18:56:32 +0200

    Add tests for validate_date

--- receipteval/tests/test_helper.py
+++ receipteval/tests/test_helper.py
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
 from receipteval.helper import simplify_category
+from receipteval.helper import validate_date
+from datetime import datetime
 
 
 def test_simplify_category_noColon():
@@ -25,3 +27,27 @@ def test_simplify_category_twoColon():
     result = simplify_category(test_category)
 
     assert result == 'c'
+
+
+#def test_validate_date_consecutive():
+    '''TODO: must raise exception'''
+#    test_date = '20150914'
+
+#    result = validate_date(test_date)
+
+
+def test_validate_date_iso():
+    test_date = '2015-09-14'
+
+    result = validate_date(test_date)
+
+    assert result == datetime(2015, 9, 14, 0, 0)
+
+
+def test_validate_date_old_format():
+    '''TODO: must raise warning'''
+    test_date = '14.09.15'
+
+    result = validate_date(test_date)
+
+    assert result == datetime(2015, 9, 14, 0, 0)
