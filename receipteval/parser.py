812596bb096cfe6250d21bde638655faece554ea
Michael Große <mic.grosse+wikitech@googlemail.com>
2015-09-21 22:00:47 +0200

    Refactor parser

--- receipteval/parser.py
+++ receipteval/parser.py
@@ -21,6 +21,8 @@ class parser(object):
         Constructor
         '''
         self.dictionary = kwargs.get('category_dictionary', ItemCategoryDict())
+        self.purchaseIsActive = False
+        self.rc = receiptCollection()
 
     def __enter__(self):
         return self
@@ -28,9 +30,48 @@ class parser(object):
     def __exit__(self, thetype, value, traceback):
         pass
 
+    def checkLineEmptyEnough(self, line):
+        return line[0] is '' and line[2] is '' and line[3] is ''
+
+    def checkPurchaseHeader(self, line):
+        return line[0] is not '' and line[2] is not ''
+
+    def startNewPurchase(self, line):
+        date = validate_date(line[0])
+        self.purchaseIsActive = True
+        payment_method = line[3]
+        if payment_method is '':
+            payment_method = 'cash'
+        self.rc.purchases.append(Purchase(date=date,
+                                          shop=line[2],
+                                          category_dict=self.dictionary,
+                                          payment_method=payment_method,
+                                          flags=line[1]))
+
+    def handleOutOfPurchaseLine(self, line):
+        if self.checkLineEmptyEnough(line):
+            return
+
+        if self.checkPurchaseHeader(line):
+            self.startNewPurchase(line)
+        else:
+            raise RuntimeError('file badly formatted: ' + str(line))
+
+    def handleInPurchaseLine(self, line):
+        if self.checkLineEmptyEnough(line):
+            self.purchaseIsActive = False
+            return
+        if line[0] is not '':
+            raise RuntimeError('file badly formatted: ' + str(line))
+        if line[3] is '':
+            self.rc.unsane_items.append(line[2])
+        else:
+            self.rc.purchases[-1].addItem(name=line[2],
+                                          price=line[3],
+                                          count=line[1],
+                                          category=line[4])
+
     def readFile(self, file_path):
-        rc = receiptCollection()
-        purchaseIsActive = False
         with open(file_path, 'r') as receipt_file:
             csv_reader = csv.reader(receipt_file)
             firstLine = True
@@ -39,45 +80,9 @@ class parser(object):
                     firstLine = False
                     continue
 
-                # empty lines: end purchase and otherwise skip
-                if line[0] is '' and line[2] is '' and line[3] is '':
-                    purchaseIsActive = False
-                    continue
-
-                date = line[0]
-
-                # only the heading of a purchase should have content in the
-                # date field
-                if purchaseIsActive and date is not '':
-                    raise RuntimeError('file badly formatted: ' + str(line))
-
-                # start a new purchase and then read the next line
-                if not purchaseIsActive and date is not '':
-                    date = validate_date(date_text=date)
-                    purchaseIsActive = True
-                    payment_method = line[3]
-                    if payment_method is '':
-                        payment_method = 'cash'
-                    rc.purchases.append(Purchase(date=date,
-                                                 shop=line[2],
-                                                 category_dict=self.dictionary,
-                                                 payment_method=payment_method,
-                                                 flags=line[1]))
-                    continue
-
-                quantity = line[1]
-                name = line[2]
-                price = line[3]
-                category = line[4]
-                if not purchaseIsActive and (name is '' or price is '' or date is ''):
-                    raise RuntimeError('file badly formatted: ' + str(line))
-
-                if price is '':
-                    rc.unsane_items.append(name)
+                if self.purchaseIsActive:
+                    self.handleInPurchaseLine(line)
                 else:
-                    rc.purchases[-1].addItem(name,
-                                             price,
-                                             quantity,
-                                             category=category)
+                    self.handleOutOfPurchaseLine(line)
 
-        return rc
+        return self.rc
