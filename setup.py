025ca197086f7d952fd32721d05842d4effef7fb
Michael Große <mic.grosse+wikitech@googlemail.com>
2015-10-24 23:34:32 +0200

    Officially support 3.4 but not 2.6 since untested

--- setup.py
+++ setup.py
@@ -70,8 +70,9 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
     ],
 
     # What does your project relate to?
@@ -91,7 +92,7 @@ setup(
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
     extras_require = {
-        'dev': [''],
+        'dev': ['pypandoc'],
         'testing': ['coverage','pytest'],
     },
     tests_require=['pytest'],
