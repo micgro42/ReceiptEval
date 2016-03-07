24cb4508a7aa909b4afcb652a007bc5a9c21059e
Michael Große <mic.grosse+wikitech@googlemail.com>
2016-03-07 23:23:25 +0100

    Fix layout with javascript

--- web/js/script.js
+++ web/js/script.js
@@ -4,9 +4,23 @@ jQuery(function(){
 deform.load();
 
 var chosen_config = {
-    'search_contains': true
+    'search_contains': true,
+    'width': '100%'
 };
 
-jQuery('.deformSeqAdd').click(function(event){jQuery('select').chosen(chosen_config);});
-jQuery('select').chosen(chosen_config);
+jQuery('.deformSeqAdd').click(function(event){jQuery('#purchaseForm select').chosen(chosen_config);});
+jQuery('#purchaseForm select').chosen(chosen_config);
+jQuery('#itemForm select').chosen({
+    'search_contains': true,
+    'width': 'auto'
+});
+jQuery('#itemForm').addClass('form-inline');
+jQuery('#categoryForm').addClass('form-inline');
+jQuery('fieldset').each(function(index){
+$( this ).children().slice(1).wrapAll('<div class="panel-body">');
+});
+jQuery('fieldset').addClass('panel panel-default');
+jQuery('legend').replaceWith(function(){
+    return jQuery("<div />").append($(this).contents()).addClass('panel-heading');
+});
 })
